754b76c3edd29f3cdcec2c1beaf262167d25c489
Erwin Rooijakkers <erwin@rooijakkers.software>
2019-02-10 17:32:00 +0100

    Initial multiple clocks with time difference

--- resources/leiningen/new/reagent/core.cljs
+++ resources/leiningen/new/reagent/core.cljs
@@ -16,14 +16,17 @@
 (defn greeting [message]
   [:h1 message])
 
-(defn clock []
-  (let [time-str (-> @timer
-                     .toTimeString
-                     (clojure.string/split " ")
-                     first)]
-    [:div.example-clock
-     {:style {:color @time-color}}
-     time-str]))
+(defn add-hours [date hours]
+  (let [current-hours (.getHours date)]
+    (doto date
+      (.setHours (+ current-hours hours)))))
+
+(defn clock [city time-str]
+  [:div.example-clock
+   [:div (str city ": ")]
+   [:div
+    {:style {:color @time-color}}
+    time-str]])
 
 (defn color-input []
   [:div.color-input
@@ -33,10 +36,30 @@
             :on-change (fn [event]
                          (reset! time-color (-> event .-target .-value)))}]])
 
+(def time-differences
+  {"London" -1
+   "Abu Dhabi" 3
+   "Paris" 0
+   "St Petersburg" 2
+   "Shanghai" 7
+   "Minneapolis" -7
+   "Montreal" -6
+   "Glasgow" -1})
+
+(defn make-time-str [datetime]
+  (-> datetime
+      .toTimeString
+      (clojure.string/split " ")
+      first))
+
 (defn page []
   [:div
    [greeting "Hello world, it is now"]
-   [clock]
+   (for [[city time-diff]]
+     (let [time-str (-> @timer
+                        (add-hours time-diff)
+                        make-time-str)]
+       [clock city time-str]))
    [color-input]])
 
 (r/render-component [page] (.querySelector js/document "#content"))
