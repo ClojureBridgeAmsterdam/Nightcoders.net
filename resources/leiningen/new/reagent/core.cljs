0f48635a3fe0f83d361037e08073d199b0c13520
Erwin Rooijakkers <erwin@rooijakkers.software>
2019-02-26 20:11:52 +0100

    js.window -> js/window

--- resources/leiningen/new/reagent/core.cljs
+++ resources/leiningen/new/reagent/core.cljs
@@ -32,7 +32,7 @@
                          (reset! time-color (-> event .-target .-value)))}]])
 
 (def base-url
-  (str (-> js.window .-location .-protocol)
+  (str (-> js/window .-location .-protocol)
        "//"
        (-> js/window .-location .-host)
        "/flags/"))
